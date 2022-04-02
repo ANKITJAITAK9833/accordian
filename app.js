ad1e9bf4a815f3623f35c4125dbf5cfa1fe3bc89
Ankit Jaitak <ankjaita@publicisgroupe.net>
2022-04-02 23:50:20 +0530

    accordian functionality- toggles other elements when opens

---
+++ app.js
@@ -0,0 +1,26 @@
+//using selectors inside the element
+const questions = document.querySelectorAll(".question");
+
+questions.forEach(function (question) {
+  const btn = question.querySelector(".question-btn");
+
+  btn.addEventListener("click", function () {
+    questions.forEach(function (item) {
+      if (item !== question) {
+        item.classList.remove("show-text");
+      }
+    });
+
+    question.classList.toggle("show-text");
+  });
+});
+
+// traversing the dom
+// const btns = document.querySelectorAll(".question-btn");
+
+// btns.forEach((btn) => {
+//   btn.addEventListener("click", (event) => {
+//     const question = event.currentTarget.parentElement.parentElement;
+//     question.classList.toggle("show-text");
+//   });
+// });
